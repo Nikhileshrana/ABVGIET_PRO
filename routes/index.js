0a8451f09f929a5ec0453f694a283ec58a7c788b
Nikhilesh Rana <realnikhileshrana@gmail.com>
2024-01-21 21:32:54 +0530

    Some Backend Changed

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HPSRLM' });
 });
 
-router.get("/:username",function(req,res){
-  console.log(`Hello, ${req.params.username} Bitch Testing Express.js`);
-  res.send(`Hi Testing the data as it is ${req.params.username}`);
-});
 
 
 router.get("",function(req,res){
