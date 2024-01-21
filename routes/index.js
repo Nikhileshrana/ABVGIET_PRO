5856be3ae08b6d73178c989646f2a2feb05f62fd
Nikhilesh Rana <realnikhileshrana@gmail.com>
2024-01-21 21:25:19 +0530

    New Design Again Bitch

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Nikhilesh Rana' });
+  res.render('index', { title: 'HPSRLM' });
 });
 
 router.get("/:username",function(req,res){
